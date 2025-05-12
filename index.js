0957ff36dc371e5140d38e923efa7ce4327551e9
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-05-12 18:40:08 -0500

    write logic to start game and display questions/options

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const jsSubject = document.querySelector("#js-subject");
 const accessSubject = document.querySelector("#access-subject");
 const subjectHeader = document.querySelector("#subject-header")
 const submitBtn = document.querySelector("#submit-btn");
+const noAnswer = document.querySelector("#no-answer");
+const questionEl = document.querySelector("#question")
+const currentQuestion = document.querySelector("#current-question");
+const totalQuestions = document.querySelector("#total-questions");
 
-const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+const answerCount = document.querySelectorAll('input[type="radio"]');
+const answers = document.querySelectorAll(".answer");
 
 
-
-console.log("Boxes: " + checkBoxes.length);
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    
+    noAnswerSelected()
 })
 
 htmlSubject.addEventListener("click", () => {
-    initializeHTML()
+    startGame(0);
 })
 
 
@@ -35,8 +38,19 @@ const fetchData = async () => {
     }
 }
 
+function noAnswerSelected() {
+    for (let x of answerCount) {
+        if (x.checked) {
+            return true;
+        } else {
+            noAnswer.style.display = "flex";
+            return false;
+        }
+    }   
+}
 
 function initializeHTML() {
+    
     main.style.display = "none";
     subjectHeader.innerHTML = 
     `<div id="html-header" class="subject-header">
@@ -45,9 +59,52 @@ function initializeHTML() {
     </div>`
 }
 
+
+async function startGame(subjectIndex) {
+    let data = await fetchData();
+    data = data.quizzes;
+
+    let index = 0;
+
+    main.style.display = "none";
+    subjectHeader.innerHTML = 
+    `<div  class="subject-header">
+        <img src="${data[subjectIndex].icon}" alt="html icon">
+        <h2>${data[subjectIndex].title}</h2>
+    </div>`
+
+    displayQuestion(subjectIndex, index);
+
+}
+
+async function displayQuestion(subjectIndex, questionIndex) {
+     let data = await fetchData();
+    data = data.quizzes;
+
+    questionEl.textContent = data[subjectIndex].questions[questionIndex].question;
+
+    currentQuestion.textContent = questionIndex + 1;
+    totalQuestions.textContent = data[subjectIndex].questions.length;
+
+
+    displayOptions(subjectIndex, questionIndex);
+}
+
+
+async function displayOptions(subjectIndex, questionIndex) {
+    let data = await fetchData();
+    data = data.quizzes;
+    let len = data[subjectIndex].questions[questionIndex].options.length;
+
+    for (let i = 0; i < len; i++) {
+        answers[i].textContent = data[subjectIndex].questions[questionIndex].options[i];
+    }
+}
+
 async function displayData() {
     let data = await fetchData();
-    data= data.quizzes;
+    data = data.quizzes;
+    console.log(data[0]);
     data.forEach((subject) => {
         console.log(subject.title);
     })
